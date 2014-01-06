b6fa5f47ecce3ffbe7f9a62d8c5d9da9cb50285d
kimika <kimika8@gmail.com>
2014-01-06 00:57:43 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CongressgameApp::Application.config.secret_key_base = '1623536a8550c75ad9dab73963ad81721341ad26163bc723489b436c591671adc53d3ebc441df66ff2c379abf10b6f22c6f5e937c8c001965050ad3b4932ab8a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CongressgameApp::Application.config.session_store :cookie_store, key: '_congressgame_app_session'
