f5238a5c067ff4713f24f6867d44f4b3684e963d
kimika <kimika8@gmail.com>
2014-01-11 19:19:49 -0500

    still trying

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 CongressgameApp::Application.routes.draw do
 
   devise_for :users
-  root :to => "welcome/index"
+  root :to => "welcome#index"
   
   get "users", to: 'user#index'
   get "politicans", to: 'politician#index'
