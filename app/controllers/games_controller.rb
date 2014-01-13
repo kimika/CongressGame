ddb71a41a92092a8ab7d60ea916213caed10f59b
kimika <kimika8@gmail.com>
2014-01-13 16:58:27 -0500

    trying to add images

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,31 @@
 class GamesController < ApplicationController
   
   def index
-  #   @games = Game.all
-  # end
+    @games = Game.all
+  end
+
+  def create
+    user.create(game.new)
+  end
+
+  def question
+    @questions = question
+  end
+
+  def answer
+    @answers = answer
+  end
+
+  def user
+    @users = user
+  end
+
+  def level
+    @levels = levels
+  end
 
   # def show
   #   @politicians = Politician params[id]
   #   @games = Game.new
   # end
-  end
 end
